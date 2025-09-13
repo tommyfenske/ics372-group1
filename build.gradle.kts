691fee40b72cda935d17d691012291b5f5a482d5
Ruben <ruben.vallejo8@gmail.com>
2025-09-12 22:53:29 -0500

    Added a filehandler class to have the user be able to open JSON files and added logic to handle user input ensuring JSON files are selected

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,12 @@ repositories {
 }
 
 dependencies {
-    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+    implementation("org.glassfish:javax.json:1.1.4")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("javax.json:javax.json-api:1.1.4")
+// Use the latest stable version
 }
 
 tasks.test {
