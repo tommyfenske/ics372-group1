d1f14dba124c25f3aab6cdf923dfe2e8bf80012e
tommyfenske <t.fenske619@gmail.com>
2025-09-29 13:51:19 -0500

    Deleted unnecessary comments in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,6 @@ import ordersystem.ExportFile;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Program Started.");
-
-        //OrderManager orderManager = new OrderManager();
         TerminalInterface terminalInterface = new TerminalInterface();
-
-        //orderManager.setTerminalInterface(terminalInterface);
-
-        //ExportFile exporter = new ExportFile(orderManager);
-
-        // Test if ExportFile can talk to OrderManager
-        //exporter.testConnection();
     }
 }
